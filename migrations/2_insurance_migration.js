c48a8ae9cc4df8d33825a3a7c8ed9d4919dcf5d6
thevirgoman <b3hzad@me.com>
2020-04-16 20:09:26 +0430

    init

---
+++ migrations/2_insurance_migration.js
@@ -0,0 +1,10 @@
+//Insurance Contract
+const Insurance = artifacts.require("Insurance");
+//Token Contract
+const CRN = artifacts.require("CoronaToken");
+
+module.exports = (deployer) => {
+  deployer.deploy(CRN).then(() => {
+    return deployer.deploy(Insurance, 15, 100, CRN.address);
+  });
+};
